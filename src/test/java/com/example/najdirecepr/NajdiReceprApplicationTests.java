5fdc548aed3d65d27aac92eb257c507190dce306
77satyr77 <135996721+77satyr77@users.noreply.github.com>
2024-06-09 10:03:20 +0200

    Add files via upload

---
+++ src/test/java/com/example/najdirecepr/NajdiReceprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.najdirecepr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NajdiReceprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
